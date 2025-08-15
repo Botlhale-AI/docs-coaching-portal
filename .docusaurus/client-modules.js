a103f415e18427c7a3f41745910101235213bc28
madlalakhaya25 <madlalakhaya@gmail.com>
2025-08-15 12:57:40 +0200

    Remove .docusaurus folder from git tracking

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/athi/Desktop/docs-coaching-portal/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/athi/Desktop/docs-coaching-portal/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/athi/Desktop/docs-coaching-portal/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/athi/Desktop/docs-coaching-portal/src/css/custom.css"),
-  require("/Users/athi/Desktop/docs-coaching-portal/src/css/navigation.css"),
-];
